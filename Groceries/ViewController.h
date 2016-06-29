72b0a1f30820b8d0f4f1ecced017177f83de94e7
Carson777 <carsoncapper@gmail.com>
2016-06-29 14:13:23 -0500

    Initial Commit

---
+++ Groceries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Groceries
+//
+//  Created by Carson Capper on 6/29/16.
+//  Copyright © 2016 Carson Capper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Groceries/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Groceries
+//
+//  Created by Carson Capper on 6/29/16.
+//  Copyright © 2016 Carson Capper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
